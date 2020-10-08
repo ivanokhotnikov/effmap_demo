084f68dc6ec0990f561ca00ce1f3198c260470ab
Okhotnikov <iokhotnikov@hydreco.com>
2020-10-08 11:39:39 +0100

    Email in setup.sh corrected

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"iglovikov@gmail.com\"\n\
+email = \"ivan.okhotnikov@outlook.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
